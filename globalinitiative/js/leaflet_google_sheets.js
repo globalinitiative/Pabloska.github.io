711a976453acfe7d0d2d4a971ae0896d6b7d4537
globalinitiative <43697566+globalinitiative@users.noreply.github.com>
2018-10-01 12:57:24 +0200

    sdfd
    dfdf

--- globalinitiative/js/leaflet_google_sheets.js
+++ globalinitiative/js/leaflet_google_sheets.js
@@ -5,8 +5,8 @@ var code = "1EXl0ZGn4RMBtim_c4zwA_te3VQgIquVywZlT_XKR0qo"
 
 document.addEventListener('DOMContentLoaded',function(){
   map = L.map('map').setView([22, 11], 4);
-  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; Global Initiative | Pablo Gallego'
+   L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   
    var marker1 = {
